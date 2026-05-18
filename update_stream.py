be907843d6c72d7045d9a1fc8d4f3b57c1225df4
By BoZo <36104762+Tahir2020@users.noreply.github.com>
2026-05-18 18:17:35 +0200

    Update update_stream.py

--- update_stream.py
+++ update_stream.py
@@ -358,11 +358,12 @@ def write_single_channel_file(channel: Dict, stream_url: str, output_folder: Pat
 
     # SADECE Show Türk
     if is_show_turk_name(name):
-        content = (
-            "#EXTM3U\n"
-            f"#EXTINF:0,{name}\n"
-            f"{stream_url}\n"
-        )
+    content = (
+        "#EXTM3U\n"
+        "#EXTVLCOPT:http-referrer=https://www.showturk.com.tr/\n"
+        "#EXTINF:-1,Show Türk\n"
+        f"{stream_url}\n"
+    )
 
     # SADECE Kanal Euro D
     elif name == "Kanal_Euro_D":
